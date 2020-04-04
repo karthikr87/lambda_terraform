de48acad96becd23ce3cc666874a0c009c3e06e5
karthikr87 <karthikeyanr.87@gmail.com>
2020-04-04 20:44:34 +0530

    send files

--- switchrole.sh
+++ switchrole.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-switch_role=`aws sts assume-role --role-arn "arn:aws:iam::638987496525:role/lambda_sqs_role" --role-session-name "lambda-role" > accessKey.json`
+switch_role=`aws sts assume-role --role-arn "$1" --role-session-name "lambda-role" > accessKey.json`
 if [[ switch_role ]]; then
   export AWS_ACCESS_KEY_ID=$(cat accessKey.json | jq '.Credentials.AccessKeyId' | tr -d '"')
   export AWS_SECRET_ACCESS_KEY=$(cat accessKey.json | jq '.Credentials.SecretAccessKey' | tr -d '"')
   export AWS_SESSION_TOKEN=$(cat accessKey.json | jq '.Credentials.SessionToken' | tr -d '"')
+  aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+  aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+  aws configure set aws_session_token $AWS_SESSION_TOKEN
 fi
